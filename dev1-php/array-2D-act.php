b285a21186a0cc952f85eed23bda4394ff8e1f5a
kredo-it-marc <83500146+kredo-it-marc@users.noreply.github.com>
2023-11-17 21:11:34 +0800

    Day 12

--- dev1-php/array-2D-act.php
+++ dev1-php/array-2D-act.php
@@ -11,8 +11,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Japanese Regions</title>
+    <style>
+        table, th, td
+        {
+            border: 1px solid black;
+        }
+
+        table{
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
-    
+    <?php
+        foreach($jap_regions as $region => $prefecture_city)
+        {
+            echo "<h1>$region</h1>";
+
+            echo "<table>";
+            echo "<tr><th>Prefecture</th><th>City</th></tr>";
+            foreach($prefecture_city as $prefecture => $city)
+            {
+                echo "<tr><td>$prefecture</td><td>$city</td></tr>";
+            }
+            echo "</table>";
+        }    
+    ?>
 </body>
 </html>
